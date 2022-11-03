a0840ce94b779006d236d536cadb466feb584d4b
raeganfaith <mynameisraeganfaith@gmail.com>
2022-11-04 01:07:43 +0800

    added bouquet pop up and auto scroll to top

--- src/assets/datepicker.js
+++ src/assets/datepicker.js
@@ -18,16 +18,78 @@ function ChangeLarge() {
   document.getElementById("change").innerHTML = "30";
 }
 
-// pop-up JavaScript
-function PopUpSleeve1(){
+// PopUps JavaScript
+// PopUps for Custom Flower
+function PopUpFlower1(){
   document.getElementById("popup-sleeve-1").classList.toggle("active");
 }
-function PopUpSleeve2(){
+function PopUpFlower2(){
+  document.getElementById("popup-flower-2").classList.toggle("active");
+}
+function PopUpFlower3(){
+  document.getElementById("popup-flower-3").classList.toggle("active");
+}
+function PopUpFlower4(){
+  document.getElementById("popup-flower-4").classList.toggle("active");
+}
+function PopUpFlower5(){
+  document.getElementById("popup-flower-5").classList.toggle("active");
+  window.scrollTo(0,0);
+}
+function PopUpFlower6(){
+  document.getElementById("popup-flower-6").classList.toggle("active");
+  window.scrollTo(0,0);
+}
+function PopUpFlower7(){
+  document.getElementById("popup-flower-7").classList.toggle("active");
+  window.scrollTo(0,0);
+}
+function PopUpFlower8(){
+  document.getElementById("popup-flower-8").classList.toggle("active");
+  window.scrollTo(0,0);
+}
+
+// PopUps for Arranged Flower Bouquet
+function PopUpBouquet1(){
+  document.getElementById("popup-bouquet-1").classList.toggle("active");
+}
+function PopUpBouquet2(){
+  document.getElementById("popup-bouquet-2").classList.toggle("active");
+}
+function PopUpBouquet3(){
+  document.getElementById("popup-bouquet-3").classList.toggle("active");
+}
+function PopUpBouquet4(){
+  document.getElementById("popup-bouquet-4").classList.toggle("active");
+}
+function PopUpBouquet5(){
+  document.getElementById("popup-bouquet-5").classList.toggle("active");
+  window.scrollTo(0,0);
+}
+function PopUpBouquet6(){
+  document.getElementById("popup-bouquet-6").classList.toggle("active");
+  window.scrollTo(0,0);
+}
+function PopUpBouquet7(){
+  document.getElementById("popup-bouquet-7").classList.toggle("active");
+  window.scrollTo(0,0);
+}
+function PopUpBouquet8(){
+  document.getElementById("popup-bouquet-8").classList.toggle("active");
+  window.scrollTo(0,0);
+}
+// PopUps for Custom Sleeves
+function PopUpFlower1(){
+  document.getElementById("popup-sleeve-1").classList.toggle("active");
+}
+function PopUpFlower2(){
   document.getElementById("popup-sleeve-2").classList.toggle("active");
 }
-function PopUpSleeve3(){
+function PopUpFlower3(){
   document.getElementById("popup-sleeve-3").classList.toggle("active");
 }
-function PopUpSleeve4(){
+function PopUpFlower4(){
   document.getElementById("popup-sleeve-4").classList.toggle("active");
 }
+
+// See More auto scroll
