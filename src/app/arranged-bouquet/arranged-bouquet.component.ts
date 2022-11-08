9382ff0b60df3905ec35b340f077bbc8e7dad6bc
raeganfaith <mynameisraeganfaith@gmail.com>
2022-11-09 01:41:20 +0800

    fixed fetching of json on arranged bouquet comp

--- src/app/arranged-bouquet/arranged-bouquet.component.ts
+++ src/app/arranged-bouquet/arranged-bouquet.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import orderData from '../bouquet.json';
+interface Bouquet {
+  image: string;
+  bouquetName: string;
+  price: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-arranged-bouquet',
@@ -9,7 +16,9 @@ export class ArrangedBouquetComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  bouquets:Bouquet[]=orderData;
+
+  
 }
